50d168828b7e780246b84c458780f76a8fa65507
hmJ4Zor-Q-3 <129763409+hmJ4Zor-Q-3@users.noreply.github.com>
2024-05-06 10:36:30 -0400

    Add Variable tests.

--- Command Runner/Variable.cs
+++ Command Runner/Variable.cs
@@ -12,19 +12,29 @@
 
         public Variable(string name, Type? type, object? value)
         {
+            TypeCheck(type, value);
             Name = name;
             Value = value;
             Type = type;
-#warning, fail fast if type wrong
         } // end constructor
 
 
 
         public void Update(Type? type, object? value) 
         {
+            TypeCheck(type, value);
             Type = type;
             Value = value;
         } // end Update()
+
+
+
+        private static void TypeCheck(Type? type, object? value)
+        {
+            bool eitherNull = (type == null || value == null);
+            if ((eitherNull && type != value?.GetType()) || (!eitherNull && !type.IsAssignableFrom(value?.GetType())))
+                throw new TypeMismatchException($"Variable type mismatch, got: {type}, expected: {value?.GetType()}");
+        } // end NullCheck
 
     } // end class
 } // end namespace
---
+++ CommandRunnerTests/TestVariable.cs
@@ -0,0 +1,53 @@
+﻿using GSR.CommandRunner;
+
+namespace GSR.Tests.CommandRunner
+{
+    [TestClass]
+    public class TestVariable
+    {
+        [TestMethod]
+        [DataRow("d", null, null)]
+        [DataRow("asSsOpe", typeof(int), 0)]
+        public void TestValidConstruct(string name, Type? type, object? value) 
+        {
+            Variable v = new(name, type, value);
+            Assert.AreEqual(v.Name, name);
+            Assert.AreEqual(v.Type, type);
+            Assert.AreEqual(v.Value, value);
+        } // end TestValidConstruct()
+
+        [TestMethod]
+        public void TestSubtypeConstruct() 
+        {
+            string name = "_9";
+            Type? type = typeof(Exception);
+            object? value = new InvalidSyntaxException();
+        
+            Variable v = new(name, type, value);
+            Assert.AreEqual(v.Name, name);
+            Assert.AreEqual(v.Type, type);
+            Assert.AreEqual(v.Value, value);
+         }
+
+        [TestMethod]
+        [ExpectedException(typeof(TypeMismatchException))]
+        [DataRow("d", typeof(string), null)]
+        [DataRow("903kf", null, "")]
+        [DataRow("4", typeof(int), 0f)]
+        public void TestTypeMismatchConstruct(string name, Type? type, object? value) => new Variable(name, type, value);
+
+        [TestMethod]
+        [ExpectedException(typeof(TypeMismatchException))]
+        public void TestSupertypeConstruct()
+        {
+            string name = "=";
+            Type? type = typeof(InvalidSyntaxException);
+            object? value = new Exception();
+
+            new Variable(name, type, value);
+        } // end TestSupertypeConstruct()
+
+        // repeat ^ except with Update..()
+
+    } // end class
+} // end namespace
